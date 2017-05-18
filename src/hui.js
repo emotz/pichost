371ca76b87407580f17e2caa20bd1f93da04956d
Tekukatetetomo <NoD-SD@yandex.ru>
2017-05-18 19:45:40 +0700

    19^45

--- src/hui.js
+++ src/hui.js
@@ -8,10 +8,7 @@ app.get('/', huy);
 
 app.get('/:name', function (req, res) {
     res.sendFile(path.join(__dirname + '/..', '/assets', req.params.name), function (err) {
-        if (err) {
-            next(err);
-        } else {
-            console.log('Sent:', path.join(__dirname + '/..', '/assets', '/1.html'));
+        if (err) {  console.log("err");
         }
     });
 });
@@ -27,13 +24,8 @@ app.listen(8080, function () {
 function huy(req, res) {
     console.log("Got request!");
     console.log(req);
-    res.sendFile(path.join(__dirname + '/..', '/assets', '/1.html'), function (err) {
-        if (err) {
-            next(err);
-        } else {
-            console.log('Sent:', path.join(__dirname + '/..', '/assets', '/1.html'));
+    res.sendFile(path.join(__dirname + '/..', '/assets', '/3.html'), function (err) {
+        if (err) { console.log('err');
         }
     });
 }
-
-console.log(path.join(__dirname + '/..', '/assets', '/1.html'));
